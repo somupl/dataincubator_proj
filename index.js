28b7368248c58073a27f631197f54c716103132a
Somu Palaniappan <somu.pl@gmail.com>
2018-05-25 05:46:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/capstone_project');
+  response.render('pages/Voice_of_Emotions');
 });
 
 app.listen(app.get('port'), function() {
