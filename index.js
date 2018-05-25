1c6d2e5e50fa3e2af3820a7b2f4a4abc9f28bed3
Somu Palaniappan <somu.pl@gmail.com>
2018-05-25 05:42:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/project_proposal');
+  response.render('pages/capstone_project');
 });
 
 app.listen(app.get('port'), function() {
